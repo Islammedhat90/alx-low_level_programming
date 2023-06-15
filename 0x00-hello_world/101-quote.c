267dfe8fa1e3c3f4f1580520fa54511834066927
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-15 12:10:52 -0700

    latest

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int size = sizeof(sentence);
 	int i;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		putchar(sentence[i]);
 	}
