7be8b241a7d44960be08cdb141f477516ab36f42
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-15 12:20:13 -0700

    latest

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-/*
+/**
  * main - program that prints exactly and that piece of art is useful
  *
+ * Description: print a quote using a for loop and function putchar
  * Return: 1 (success)
  */
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char sentence[] =
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int size = sizeof(sentence);
 	int i;
-	for (i = 0; i < size - 1; i++)
-	{
-		putchar(sentence[i]);
-	}
-	putchar('\n');
-	return (1);
+for (i = 0; i < size - 1; i++)
+{
+	putchar(sentence[i]);
+}
+putchar('\n');
+return (1);
 }
