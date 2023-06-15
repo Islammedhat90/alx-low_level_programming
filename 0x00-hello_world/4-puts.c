e4e785a6126b476c13f67f115417eb413efd32f9
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-15 10:20:52 -0700

    latest

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: String s (Success)
+ */
+int main(void)
+{	
+    puts("\"Programming is like building a multilingual puzzle");	    
+    return (0);
+}
