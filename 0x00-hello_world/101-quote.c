3423937c12a77d93f4afbb75826804e47bad9a6e
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-15 11:28:46 -0700

    latest

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - program that prints exactly and that piece of art is useful
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char sentence[] = "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19";
+	int len = strlen(sentence);
+
+	for (int i = 0; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n);
+	return (0);
+}
