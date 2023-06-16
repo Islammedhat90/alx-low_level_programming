ae25e07ad272828405209eab9edc22df5a436342
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-16 05:51:01 -0700

    altest

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - print all numbers base 16
+  *
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	char c = '0';
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		if (c == '9')
+		{
+			c = '141';
+			continue;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
