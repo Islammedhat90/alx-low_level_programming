bc55306105ae2e07bae78b611a216fadf2b6a9e9
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-20 03:01:25 -0700

    latest

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,23 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
+
+
+
+
+
+
+
+
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
