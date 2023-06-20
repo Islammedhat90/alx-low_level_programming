64b8835d45d78b0eb86643ea3fc3418687516117
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-20 07:33:41 -0700

    latest

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - add two integer;
+  * @n1: first integer @n2: second integer
+  *
+  * Return: int result:
+  */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
