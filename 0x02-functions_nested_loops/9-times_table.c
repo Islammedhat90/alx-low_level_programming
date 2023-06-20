6bc12cf197f81369f14322923804ece6a4bf9ca2
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-20 05:54:20 -0700

    latest

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * times_table - prints the 9 times table.
+  *
+  * Return: nothing.
+  */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int x = i * j;
+			_putchar(x + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
