ab468b09220e5b63a602cf6ad16162ddccb08930
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-20 08:12:57 -0700

    latest

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,14 +26,20 @@ void print_times_table(int n)
 				}
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
 				if (x > 9)
 				{
+					_putchar(' ');
 					_putchar((x / 10) + '0');
 					_putchar((x % 10) + '0');
 				}
+				else if (x > 99)
+				{
+					_putchar((x / 100) + '0');
+					_putchar(((x / 10) % 10) + '0');
+					_putchar((x % 10) + '0');
 				else
 				{
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(x + '0');
 				}
