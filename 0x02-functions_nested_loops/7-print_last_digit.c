f2903c7632a9da904885d78de5bd84f712cab8b2
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-20 05:01:16 -0700

    latest

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
   * print_last_digit - print last digit of number
-  * @n: number 
+  * @n: number
   *
   * Return: number last digit.
   */
@@ -10,8 +10,9 @@
 int print_last_digit(int n)
 {
 	int r = n % 10;
-	if (n < 0 )
-		r = (-1 * n) % 10;
+
+	if (n < 0)
+		r = -1 * (n % 10);
 	_putchar(r + '0');
 	return (r);
 }
