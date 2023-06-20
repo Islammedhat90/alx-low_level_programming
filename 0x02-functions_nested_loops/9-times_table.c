f7a8922776423b0669dbd3762b080fa38d72c651
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-20 06:08:45 -0700

    latest

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,14 +18,18 @@ void times_table(void)
 			int x = i * j;
 
 			if (x > 9)
+			{
 				_putchar((x / 10) + '0');
 				_putchar((x % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
+			}
 			else
-				_putchar(x + '0')
+			{
+				_putchar(x + '0');
 				_putchar(',');
-				_putchar(' ');	
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
