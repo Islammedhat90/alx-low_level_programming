dd6a69be6e5620c08fe7cf6203f65ac9d926dac8
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-22 12:38:58 -0700

    latest

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - C function that multiplies two integers.
+* @a: first number
+* @b: second number
+*
+* Return: returns value of multiplication.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_line - function that draws a straight line in the terminal.
+  * @n: The number of lines to print
+  *
+  * Return: nothing
+  */
+
+ void print_line(int n)
+ {
+    if (n <= 0)
+    {
+         _putchar('\n');
+    }
+    else
+    {
+        int i = 0;
+
+        while (i < n)
+        {
+            _putchar('_');
+            i++;
+        }
+        _putchar('\n');
+    }
+ }
