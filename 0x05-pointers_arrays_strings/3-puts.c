fd94d510bbd3a2dc8f7dc45a34f468006840cbe5
Islammedhat90 <islam.medhat.h@gmail.com>
2023-06-26 04:29:35 -0700

    latest

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		*str++;
+	}
+}
