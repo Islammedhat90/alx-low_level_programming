c2966f91122679c94d6308a849a67a4163b24566
Islammedhat90 <islam.medhat.h@gmail.com>
2023-07-03 02:31:12 -0700

    latest

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _memset - function that fills memory with a constant byte
  *@s: address to start
@@ -9,7 +9,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
