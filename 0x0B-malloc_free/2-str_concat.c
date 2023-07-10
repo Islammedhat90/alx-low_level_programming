6ac0396daaeeffd5ab2b2871fbf54bece499efd6
Islammedhat90 <islam.medhat.h@gmail.com>
2023-07-10 06:21:12 -0700

    latest

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	char *new;
 	int i;
 	int j;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+  *@width: width of grid.
+  *@height: height of grid.
+  *
+  * Return: NULL on failure, otherwise return pointer.
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+	int x;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		grid[x] = malloc(width * sizeof(int));
+		if (grid[x] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
