31c39b35becdba5f46009cbe8c6360e257a68834
Islammedhat90 <islam.medhat.h@gmail.com>
2023-07-10 05:51:30 -0700

    latest

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,18 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
+	int len1;
+	int len2;
 	char *new;
 	int i;
 	int j;
-
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 	new = malloc(len1 + len2 + 1);
 	if (new == NULL)
 		return (NULL);
