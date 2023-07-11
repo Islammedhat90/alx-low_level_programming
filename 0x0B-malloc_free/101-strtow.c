a0e80cf4da6de9b40ddb32d584228aeae100b61a
Islammedhat90 <islam.medhat.h@gmail.com>
2023-07-11 08:52:13 -0700

    latest

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,43 +1,112 @@
-#include <stdlib.h>
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-* **strtow - function that splits a string into words.
-* @str : pointer
-* Return: Char
-*/
+  * wordcount - counts number of words in a string
+  *@str: string to work on.
+  *
+  * Return: int with number of words.
+  */
+
+int wordcount(char *str)
+{
+	int words = 0;
+	int i = 0;
+	int size = strlen(str);
+
+	for (i = 0; i < size; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			words++;
+	}
+	return (words);
+}
+
+/**
+  * lettercount - array of number of letters of each word
+  *@str: sentence to analyse.
+  *
+  * Return: array of integers.
+  */
+
+int *lettercount(char *str)
+{
+	int size = strlen(str);
+	int words = wordcount(str);
+	int i;
+	int let = 0;
+	int j = 0;
+	int *letters = malloc(words * sizeof(int));
+
+	if (letters == NULL)
+	{
+		free(letters);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (str[i] != ' ')
+		{
+			let++;
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+			{
+				letters[j] = let;
+				j++;
+				let = 0;
+			}
+		}
+	}
+	return (letters);
+}
+
+/**
+  * strtow - splits a string into words.
+  *@str: string to split.
+  *
+  * Return: pointer to split words.
+  */
 
 char **strtow(char *str)
 {
-	char **d;
+	int *letters = lettercount(str);
+	int words = wordcount(str);
+	int size = strlen(str);
 	int i;
-	int j = 0;
-	int con = 0;
-
-	if (str == NULL)
+	int j;
+	char **s;
+	int index = 0;
+	if (str == NULL || (strcmp(str, "") == 0))
 		return (NULL);
+	s = malloc((words + 1) * sizeof(char *));
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+	for (i = 0; i < words; i++)
 	{
-		if (str[i] != 32)
-			con++;
-	}
-
-	d = malloc(sizeof(char) * con);
-
-	if (d == NULL)
-		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] != 32)
+		s[i] = malloc((letters[i] + 1) * sizeof(char));
+		if (s[i] == NULL)
 		{
-			*d[j] = str[i];
-			j++;
+			for (; i >= 0; i--)
+				free(s[i]);
+			free(s);
+			return (NULL);
 		}
-		else
+		for (j = index; j < size; j++)
 		{
+			if (str[j] != ' ')
+			{
+				s[i][j - index] = str[j];
+
+				if (str[j + 1] == ' ' || str[j + 1] == '\0')
+				{
+					s[i][j - index] = '\0';
+					break;
+				}
+				continue;
+			}
+			continue;
 		}
 	}
-	return (d);
+	s[words] = NULL;
+	return (s);
 }
--- 0x0B-malloc_free/102-strtow.c
+++ 0x0B-malloc_free/102-strtow.c
@@ -18,9 +18,139 @@ int wordcount(char *str)
 
 	for (i = 0; i < size; i++)
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+		if (str[i] != ' '
 			words++;
 	}
 	return (words);
 }
+
+/**
+  * lettercount - array of number of letters of each word
+  *@str: sentence to analyse.
+  *
+  * Return: array of integers.
+  */
+
+int *lettercount(char *str)
+{
+	int size = strlen(str);
+	int words = wordcount(str);
+	int i;
+	int let = 0;
+	int j = 0;
+	int *letters = malloc(words * sizeof(int));
+
+	if (letters == NULL)
+	{
+		free(letters);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (str[i] != ' ')
+		{
+			let++;
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+			{
+				letters[j] = let;
+				j++;
+				let = 0;
+			}
+		}
+	}
+	return (letters);
+}
+
+/**
+  * strtow - splits a string into words.
+  *@str: string to split.
+  *
+  * Return: pointer to split words.
+  */
+
+char **strtow(char *str)
+{
+	if (str == NULL || (strcmp(str, "") == 0))
+		return (NULL);
+	int *letters = lettercount(str);
+	int words = wordcount(str);
+	int i;
+	int j;
+	int index = 0
+	int **s = (char **)malloc((words + 1) * sizeof(char *));
+
+	for (i = 0; i < words; i++)
+	{
+		s[i] = malloc((letters[i] + 1) * sizeof(char));
+		if (s[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(s[i]);
+			free(s);
+			return (NULL);
+		}
+		for (j = index; j < size; j++)
+		{
+			if (str[j] ! = ' ')
+			{
+				s[i][j - index] = str[j];
+				
+				if (str[j + 1] == ' ' || str[j + 1] == '\0')
+				{
+					s[i][j - index] = '\0';
+					break;
+				}
+				continue;
+			}
+			continue;
+		}
+	}
+	s[words] == NULL;
+	return (s);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
