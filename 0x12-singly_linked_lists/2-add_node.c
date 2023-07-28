0bf3c01ea36e4f8e487d667e56398db542f361c7
Islammedhat90 <islam.medhat.h@gmail.com>
2023-07-28 07:56:55 -0700

    latest

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,12 +15,12 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL || head == NULL)
+	if (!head || !new)
 		return (NULL);
 	if (str)
 	{
 		new->str = strdup(str);
-		if (new->str == NULL)
+		if (!new->str)
 		{
 			free(new);
 			return (NULL);
