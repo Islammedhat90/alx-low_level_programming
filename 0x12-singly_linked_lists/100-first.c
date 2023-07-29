d6975e17b2115fd41ec653b70d12c9952a237664
Islammedhat90 <islam.medhat.h@gmail.com>
2023-07-28 17:58:43 -0700

    latest

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * before - print before main
+ * Return: no return.
+ */
+
+void __attribute__((constructor)) before()
+{
+	printf("You're beat! and yet, you must allow,"
+	"\nI bore my house upon my back!\n");
+}
