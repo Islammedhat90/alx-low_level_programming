f2ddbbfb5f27183d851af4f33e07a45fd215479f
Islammedhat90 <islam.medhat.h@gmail.com>
2023-07-28 17:44:54 -0700

    latest

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of the linked list.
+ * Return: Always 0.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *curr = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		free(curr->str);
+		free(curr);
+		curr = head;
+	}
+}
