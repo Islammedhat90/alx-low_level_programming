6a82092e875506a80c42edf22421a6d97cb3150f
Islammedhat90 <islam.medhat.h@gmail.com>
2023-08-01 17:22:51 -0700

    latest

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,8 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp;
 	listint_t *current;
 
-	temp = malloc(sizeof(listint_t));
-	if (*head == NULL || temp == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 	current = *head;
 	if (index == 0)
