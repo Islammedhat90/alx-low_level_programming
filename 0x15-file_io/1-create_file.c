34e5991caa01e7d4002b82e01444b3795448db38
Islammedhat90 <islam.medhat.h@gmail.com>
2023-08-11 13:48:16 -0700

    latest

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int l;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, 0_WRONLY | 0_CREAT | 0_TRUNC, 0600);
+	if (text_content != NULL)
+		l = write(fd, text_content, _strlen(text_content));
+	if (l == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
